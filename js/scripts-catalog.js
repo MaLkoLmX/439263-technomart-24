8537f8805cae8b1d3d6f01abef25c50239a9ac7d
MaLkoLm_X <jekyboyinhell@gmail.com>
2019-02-28 15:39:10 +0400

    Исправления

---
+++ js/scripts-catalog.js
@@ -0,0 +1,24 @@
+var addItem = document.querySelectorAll(".catalog-button");
+var add = document.querySelector(".modal-add");
+var addClose = document.querySelector(".add__close");
+
+addItem.forEach(function(addItem) {
+  addItem.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    add.classList.add("modal__show");
+  });
+});
+
+addClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  add.classList.remove("modal__show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (add.classList.contains("modal__show")) {
+      add.classList.remove("modal__show");
+    }
+  }
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
 var link = document.querySelector(".contacts__button");
+
 var popup = document.querySelector(".modal__form");
 var close = document.querySelector(".button-close");
 
+var form = popup.querySelector("form");
 var login = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
 var text = popup.querySelector("[name=text]");
-var form = popup.querySelector("form");
-
-var addItem = document.querySelectorAll(".catalog-button");
-var add = document.querySelector(".modal-add");
-var addClose = document.querySelector(".add__close");
 
 var isStorageSupport = true;
 var storage = "";
+
+var mapLink = document.querySelector(".contacts__map-modal");
+var map = document.querySelector(".modal__map");
+var mapClose = map.querySelector(".modal__map-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  map.classList.add("modal__show-map");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  map.classList.remove("modal__show-map");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (map.classList.contains("modal__show-map")) {
+      map.classList.remove("modal__show-map");
+    }
+  }
+});
 
 try {
   storage = localStorage.getItem("login");
@@ -43,7 +63,6 @@ form.addEventListener("submit", function (evt) {
     popup.classList.remove("modal__error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal__error");
-    console.log("Нужно ввести логин и пароль");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", login.value);
@@ -62,7 +81,7 @@ window.addEventListener("keydown", function (evt) {
 });
 
 
-var mapLink = document.querySelector(".contacts__map-modal");
+/*var mapLink = document.querySelector(".contacts__map-modal");
 var map = document.querySelector(".modal__map");
 var mapClose = map.querySelector(".modal__map-close");
 
@@ -83,27 +102,4 @@ window.addEventListener("keydown", function (evt) {
       map.classList.remove("modal__show-map");
     }
   }
-});
-
-
-/*-------------------Добавления в корзину-------------------*/
-addItem.forEach(function(addItem) {
-  addItem.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    add.classList.add("modal__show");
-  });
-});
-
-addClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  add.classList.remove("modal__show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  evt.preventDefault();
-  if (evt.keyCode === 27) {
-    if (add.classList.contains("modal__show")) {
-      add.classList.remove("modal__show");
-    }
-  }
-});
+});*/
